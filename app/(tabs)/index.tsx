ca754fe558f567be9c9f6b6f4ad8db56c5f7ed7e
Asitha Kanchana <s23010526@ousl.lk>
2024-10-28 20:45:23 +0530

    finished v2.0

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,123 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from "react-native";
+//const logoImage = require("../../assets/images/ezShop.png");
+export default function App() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    <View style={styles.container}>
+      <Text style = {styles.EzShop}>Eazy Shopping</Text>
+    {/* Image Container..... */}
+    <View style = {styles.imageContainer}>
+      <Image 
+        source={require('../../assets/images/ezShop.png')}
+        style = {{
+          width:100,
+          height:100,
+          marginBottom: 20,
+          padding:50,
+          alignItems:'center',
+        }}
+        
+      />
+    </View>
+{/*User name input*/} 
+    <TextInput
+      style = {styles.uInput}
+      placeholder="User Name"
+      />
+      
+      <StatusBar style="auto"/>
+
+{/* Password Input  */}
+      <TextInput
+      style = {styles.pInput}
+      placeholder="Password"
+      />
+
+      {/* Button Section */}
+      <View style = {styles.buttonContainer}>
+        {/* Login Up */}
+        <TouchableOpacity style = {styles.sinUpBtn}>
+          <Text
+          style = {styles.btnText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+      <View style = {styles.buttonContainer}>
+        {/* Sing Up btn */}
+        <TouchableOpacity style = {styles.logInBtn}>
+          <Text
+          style = {styles.btnText}>Sing Up</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
 }
 
+
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  // background 
+  container: {
+    marginTop: 50,
+    backgroundColor:'lightblue',
+    flex:1,
+    alignItems:'center',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+// image container
+  imageContainer:{
+    justifyContent: 'center',
+    alignItems :'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  // image formatting
+  EzShop:{
+    marginTop:10,
+    marginBottom:40,
+    fontWeight:'bold',
+    fontSize :35,
+    textAlign:'center',
+    margin:10,
+    color:'purple',
+    fontFamily:"sans-serif",
+  },
+// User Name Input
+  uInput:{
+    width:"80%",
+    backgroundColor:'#fff',
+    padding:10,
+    marginBottom:10,
+    textAlign:'center',
+    fontWeight:'bold',
+  },
+  // Password Input 
+  pInput:{
+    width:"80%",
+    backgroundColor:'#fff',
+    padding:10,
+    marginBottom:10,
+    textAlign:'center',
+    fontWeight:'bold',
+  },
+  // sing up btn
+  sinUpBtn:{
+    backgroundColor:'lightgreen',
+    padding:15,
+    width: "100%",
+    marginBottom:10,
+  },
+  // login btn 
+  logInBtn:{
+    backgroundColor:'orange',
+    padding:15,
+    width: "100%",
+  },
+// button text formatting
+  btnText:{
+    fontSize:18,
+    textAlign:'center',
+    fontWeight:'bold',
+
+  },
+  // btn container
+  buttonContainer:{
+    flexDirection: 'row',  
+    width:"80%",
   },
 });
